a3869dd0950a5beaa531193b60b1fa27ee952ac5
Matt Smithies <info@mattsmithies.co.uk>
2020-04-25 14:40:11 +0100

    Updated sample with examples of exchange amounts with a value and a error returning.

--- sample.php
+++ sample.php
@@ -7,4 +7,10 @@ use Changelly\Changelly;
 // get your key and secret here https://changelly.com/developers#keys
 $changelly = new Changelly('yourApiKey', 'yourApiSecret');
 
-echo json_encode($changelly->getCurrenciesFull());
+echo json_encode($changelly->getCurrenciesFull()) . PHP_EOL;
+
+// Ensure that a basic conversion returns a value
+echo json_encode($changelly->getExchangeAmount('btc', 'eth', 1)) . PHP_EOL;
+
+// Return the error when changelly cannot process the request
+echo json_encode($changelly->getExchangeAmount('btc', 'eth', 22000000)) . PHP_EOL;
