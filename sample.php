fda49c8f7d9f01564d6890f65fbddf288b48edbf
Matt Smithies <flyinggazelle@hotmail.co.uk>
2020-04-28 16:15:46 +0100

    Resolve [ErrorException: Undefined property] - Return changelly errors (#3)
    * On all post requests check that result is set, else throw an error.
    
    * Updated sample with examples of exchange amounts with a value and a error try/catch.
    
    Co-authored-by: Matt Smithies <info@mattsmithies.co.uk>

--- sample.php
+++ sample.php
@@ -7,4 +7,15 @@ use Changelly\Changelly;
 // get your key and secret here https://changelly.com/developers#keys
 $changelly = new Changelly('yourApiKey', 'yourApiSecret');
 
-echo json_encode($changelly->getCurrenciesFull());
+echo json_encode($changelly->getCurrenciesFull()) . PHP_EOL;
+
+// Ensure that a basic conversion returns a value
+echo json_encode($changelly->getExchangeAmount('btc', 'eth', 1)) . PHP_EOL;
+
+// Throw an error, with a status code of 422, as the request cannot be processed
+try {
+    $changelly->getExchangeAmount('btc', 'eth', 22000000);
+} catch (Exception $e) {
+    echo $e->getCode() . PHP_EOL;
+    echo $e->getMessage() . PHP_EOL;
+}
